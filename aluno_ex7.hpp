8bbbef64d410e56e6545ec053597c2ef9cbdbed8
Álamo <85910335+alamodepaula@users.noreply.github.com>
2021-08-04 15:13:11 -0300

    Update aluno_ex7.hpp

--- aluno_ex7.hpp
+++ aluno_ex7.hpp
@@ -9,5 +9,11 @@
 int
 exercicio7(int n)
 {
-  return 10;
+    int numFib, n;
+    if (n == 1 || n == 2)
+        return 1;
+    else
+        if (n == 0) 
+        return 0;
+    return numFib(n - 1) + numFib(n - 2);
 }
