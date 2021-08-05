7f565cf56066c499c9a9adf5a12a8c30f1945320
Álamo <85910335+alamodepaula@users.noreply.github.com>
2021-08-04 22:00:35 -0300

    Update aluno_ex10.hpp

--- aluno_ex10.hpp
+++ aluno_ex10.hpp
@@ -6,8 +6,25 @@
 
 // 10. Dois números são chamamos coprimos, se o único divisor comum aos dois é onúmero 1.
 // Faça um programa em C que, dado n e m, diga se eles são coprimos.
-int
-exercicio10(int n, int m)
-{
-  return 0;
+
+int exercicio10(int n, int m){
+    int maiorNum, menorNum, resto;
+    if(n > m){
+	maiorNum = n;	
+	menorNum = m;
+	}
+	else{
+	maiorNum = m;
+	menorNum = n;	
+	}
+	
+	while(maiorNum % menorNum != 0){
+	resto = (maiorNum % menorNum);
+	maiorNum = menorNum;
+	menorNum = resto;
+	}
+	if(menorNum == 1)
+	   return true; 
+	else
+	   return false;
 }
